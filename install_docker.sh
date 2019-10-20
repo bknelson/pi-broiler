a9206c1cf2ef3149ada2d9a05fe1b93c3c0fdd00
Ben Nelson <bknelson4@gmail.com>
2019-10-20 02:50:01 -0700

    Adding docker

---
+++ install_docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+
+# Add the Docker official repos
+echo "deb [arch=armhf] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+     $(lsb_release -cs) stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list
+
