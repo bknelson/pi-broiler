db78a37dfbb31c54d74903c82625029fb59925cd
Ben Nelson <bknelson4@gmail.com>
2019-10-19 20:19:48 -0700

    Packages needed to build pyenv

--- setup_pyenv.sh
+++ setup_pyenv.sh
@@ -2,13 +2,14 @@
 
 curl https://pyenv.run | bash
 
+echo -e "\n"
+echo '# pyenv'
 echo 'PATH="$HOME/.pyenv/bin:$PATH"' >> $HOME/.bashrc
-echo 'eval "${pyenv init -}"' >> $HOME/.bashrc
-echo 'eval "${pyenv virtaulenv-init -)"' >> $HOME/.bashrc
+echo 'eval "$(pyenv init -)"' >> $HOME/.bashrc
+echo 'eval "$(pyenv virtaulenv-init -)"' >> $HOME/.bashrc
 
 source $HOME/.bashrc
 
-pyenv install 3.7.5
-
-pyenv global 3.7.5
+pyenv install 3.8.0
+pyenv global 3.8.0
 
