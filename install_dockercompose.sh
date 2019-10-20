515a3b28e521c183d9378fe2833b4684d48b2103
Ben Nelson <bknelson4@gmail.com>
2019-10-20 03:19:18 -0700

    Changing docker compose install

--- install_dockercompose.sh
+++ install_dockercompose.sh
@@ -3,5 +3,4 @@
 sudo apt update
 sudo apt install -y python python-pip libffi-dev python-backports.ssl-match-hostname
 
-pip install --upgrade pip
-pip install -r dockercompose_requirements.txt
+sudo pip install docker-compose
